aba0401acc2a34e02733c853937109408347da52
luzpatty <chinita_310895@hotmail.com>
2017-08-19 14:24:00 -0500

    terminado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'venta/reporte'
   root 'venta#index'
+  get 'venta/reporte'
   resources :venta
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
